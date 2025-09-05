49829dc7d12ba44fc5511a69e8859ede83d8f5b7
Jacob Moody <moody@posixcafe.org>
2025-09-04 20:57:54 -0500

    remove debug log

--- src/main/java/com/majiru/bankregex/BankRegexSearchPlugin.java
+++ src/main/java/com/majiru/bankregex/BankRegexSearchPlugin.java
@@ -180,7 +180,6 @@ public class BankRegexSearchPlugin extends Plugin
 
 		int itemId = intStack[intStackSize - 1];
 		String search = stringStack[stringStackSize - 1].toString();
-        log.debug(search);
 		if (bookmark != null)
 		{
 			if (bookmarkSearch(itemId))
